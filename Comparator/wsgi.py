e9b09488a71562bf8cadef62493611ec2ea9477f
PaulGrishunin <paul_gri@mail.ru>
2021-06-15 11:29:11 +0200

    first deploy6

--- Comparator/wsgi.py
+++ Comparator/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Comparator.settings')
+os.environ.setdefault('settings.heroku', 'Comparator.settings')
 
 application = get_wsgi_application()
