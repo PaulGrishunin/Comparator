a51747f520fd08133810d14206c17345601ffc29
PaulGrishunin <paul_gri@mail.ru>
2021-06-16 21:13:21 +0200

    Diploy1

--- Comparator/settings.py
+++ Comparator/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    # 'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
