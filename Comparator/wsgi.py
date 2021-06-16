e0bf76578af4b8c7d524809ce9991da536f55f56
PaulGrishunin <paul_gri@mail.ru>
2021-06-16 20:09:39 +0200

    Diploy1

--- Comparator/wsgi.py
+++ Comparator/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Comparator.settings'
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'Comparator.settings')
 from django.core.wsgi import get_wsgi_application
 # application = get_wsgi_application()
