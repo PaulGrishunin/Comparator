a63b33238f6ab4b26f7a7013abe4d3ed905af319
PaulGrishunin <paul_gri@mail.ru>
2021-06-16 20:19:26 +0200

    Diploy1

--- Comparator/wsgi.py
+++ Comparator/wsgi.py
@@ -11,7 +11,8 @@ import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'Comparator.settings'
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'Comparator.settings')
 from django.core.wsgi import get_wsgi_application
-# application = get_wsgi_application()
-from dj_static import Cling
+application = get_wsgi_application()
 
-application = Cling(get_wsgi_application())
+# from dj_static import Cling
+#
+# application = Cling(get_wsgi_application())
