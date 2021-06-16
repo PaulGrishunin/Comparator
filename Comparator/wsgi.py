3a0cf3676c53c026732bad1a617452350d22f57d
PaulGrishunin <paul_gri@mail.ru>
2021-06-16 18:46:52 +0200

    Diploy1

--- Comparator/wsgi.py
+++ Comparator/wsgi.py
@@ -7,11 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
-import os
+#import os
 
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'Comparator.settings')
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'Comparator.settings')
 from django.core.wsgi import get_wsgi_application
 #application = get_wsgi_application()
 from dj_static import Cling
