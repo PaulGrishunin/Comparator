04406c1ad20ff9d844af475210badf5d0e79f883
PaulGrishunin <paul_gri@mail.ru>
2021-06-23 05:27:53 +0200

    Diploy4

--- Comparator/settings.py
+++ Comparator/settings.py
@@ -16,7 +16,7 @@ import django_heroku
 django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))     #BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -147,11 +147,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [                              #
-    os.path.join(BASE_DIR, 'build/static')
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [                              #
+#     os.path.join(BASE_DIR, 'build/static')
+# ]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 #активировать CORS и перечень разрешенных URL
