6609a8eecf0995fea09d5cf5d24f0250fb48d7cd
PaulGrishunin <paul_gri@mail.ru>
2021-06-23 04:56:44 +0200

    Diploy3

--- Comparator/settings.py
+++ Comparator/settings.py
@@ -16,7 +16,7 @@ import django_heroku
 django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().root.root              # BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
