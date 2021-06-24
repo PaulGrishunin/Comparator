502598f0d642645123eeea88c9a5adf3c4179648
PaulGrishunin <paul_gri@mail.ru>
2021-06-24 14:04:18 +0200

    Diploy5

--- ads/views.py
+++ ads/views.py
@@ -29,7 +29,7 @@ from django.views.decorators.cache import never_cache
 # Serve Single Page Application
 # index = never_cache(TemplateView.as_view(template_name='index.html'))
 def new_home(request):
-    return render(request, 'index.html')
+    return render(request, 'index')
 
 
 class BrandsListView(generics.ListAPIView):
