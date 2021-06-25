5efbe17cbbb2182a2589cb7f4f6c37f73e5ba9c1
PaulGrishunin <paul_gri@mail.ru>
2021-06-25 04:10:56 +0200

    Diploy5

--- Comparator/settings.py
+++ Comparator/settings.py
@@ -18,8 +18,8 @@ import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -188,8 +188,8 @@ STATIC_URL = '/static/'
 STATICFILE_DIR = os.path.join(BASE_DIR, 'build/static')
 # STATICFILE_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATICFILE_DIR,]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # If you want to serve user uploaded files add these settings
