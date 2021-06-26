4f0b5a6a0ec268d2fd4d8a91fb07f5c806213428
PaulGrishunin <paul_gri@mail.ru>
2021-06-26 04:45:02 +0200

    Deploy5

--- Comparator/settings.py
+++ Comparator/settings.py
@@ -193,7 +193,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # If you want to serve user uploaded files add these settings
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'build/static/media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'build/static/media')
 
 
